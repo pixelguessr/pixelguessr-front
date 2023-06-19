1a3a860127c0a200de2cfb58b1edac85cdee3852
pedrovonkarma <heeypedroo@gmail.com>
2023-06-18 22:57:28 -0300

    all characters created

--- src/assets/characters.js
+++ src/assets/characters.js
@@ -3,7 +3,7 @@ export const cha = [
     name: "KEN",
     fullname:"Ken",
     game:"Street Fighter",
-    image: "",
+    image: "https://i.imgur.com/F7pAjxi.png",
     hint_one: "É rival do “protagonista” da série",
     hint_two:"Usa um golpe famoso chamado “HADOUKEN”",
     hint_three:"Faz parte da franquia de jogos Street Fighter"
@@ -12,7 +12,7 @@ export const cha = [
     name: "IORI",
     fullname:"Iori Yagami",
     game:"The King Of Fighters",
-    image: "",
+    image: "https://i.imgur.com/aovqvPj.jpg",
     hint_one: "Seu poder está relacionado a chamas roxas",
     hint_two:"Foi um dos protagonistas em alguns dos jogos da série",
     hint_three:"Faz parte da franquia de The King of Fighters"
@@ -21,7 +21,7 @@ export const cha = [
     name: "KRATOS",
     fullname:"Kratos",
     game:"God of War",
-    image: "",
+    image: "https://i.imgur.com/EUqoSU4.jpg",
     hint_one: "Foi um personagem muito raivoso em sua saga original",
     hint_two:"Foi amaldiçoado e recebeu título de “Fantasma de Sparta”",
     hint_three:"Foi considerado o Deus da Guerra depois de derrotar Ares"
@@ -30,7 +30,7 @@ export const cha = [
     name: "DANTE",
     fullname:"Dante",
     game:"Devil May Cry",
-    image: "",
+    image: "https://i.imgur.com/cTEyz4l.jpg",
     hint_one: "Tem um irmão gêmeo que é abordado nos jogos",
     hint_two:"Portador uma espada chamada “Rebellion”",
     hint_three:"Protagonista da saga Devil May Cry"
@@ -39,7 +39,7 @@ export const cha = [
     name: "EZIO",
     fullname:"Ezio Auditore",
     game:"Assassin's Creed",
-    image: "",
+    image: "https://i.imgur.com/8pA926A.jpg",
     hint_one: "Nasceu na Itália",
     hint_two:"No universo do jogo, foi amigo de Leonardo da Vinci",
     hint_three:"Fez parte de um credo de assassinos"
@@ -48,7 +48,7 @@ export const cha = [
     name: "GLADOS",
     fullname:"GLaDOS",
     game:"Portal",
-    image: "",
+    image: "https://i.imgur.com/2dTyvqD.jpg",
     hint_one: "Faz muito uso da ironia e sarcasmo",
     hint_two:"É uma inteligência artificial",
     hint_three:"Maior antagonista da série Portal"
@@ -57,7 +57,7 @@ export const cha = [
     name: "LEON",
     fullname:"Leon S. Kennedy",
     game:"Resident Evil",
-    image: "",
+    image: "https://i.imgur.com/Ua1EanH.jpg",
     hint_one: "Teve um péssimo primeiro dia de trabalho",
     hint_two:"Foi policial em Raccoon City",
     hint_three:"Foi à Espanha em busca da filha do presidente, Ashley Graham"
@@ -66,7 +66,7 @@ export const cha = [
     name: "VAAS",
     fullname:"Vaas Montenegro",
     game:"Far Cry 3",
-    image: "",
+    image: "https://i.imgur.com/gdgiom5.jpg",
     hint_one: "A franquia é conhecida por ter antagonistas marcantes",
     hint_two:"Te ensina a definição de insanidade",
     hint_three:"Antagonista de um dos jogos da saga Far Cry"
@@ -75,7 +75,7 @@ export const cha = [
     name: "SONIC",
     fullname:"Sonic",
     game:"Sonic the Hedgehog",
-    image: "",
+    image: "https://i.imgur.com/pn8xV9v.png",
     hint_one: "É um ouriço azul",
     hint_two:"É um personagem muito veloz e coleta anéis",
     hint_three:"Foi por muito tempo o mascote da SEGA"
@@ -84,7 +84,7 @@ export const cha = [
     name: "LINK",
     fullname:"Link",
     game:"The Legend of Zelda",
-    image: "",
+    image: "https://i.imgur.com/MUtgASf.png",
     hint_one: "Protagonista de uma famosa saga de RPG's",
     hint_two:"Fez parte de Smash Bros",
     hint_three:"Muitas vezes chamam ele de Zelda"
@@ -93,7 +93,7 @@ export const cha = [
     name: "CRONO",
     fullname:"Crono",
     game:"Chrono Trigger",
-    image: "",
+    image: "https://i.imgur.com/sVg7DFc.png",
     hint_one: "O design do jogo foi feito por Akira Toriyama, criador de Dragon Ball",
     hint_two:"Protagonista de um RPG muito aclamado do Super Nintendo",
     hint_three:"Seu jogo envolve viagens no tempo, ou melhor, “cronológicas”"
@@ -102,7 +102,7 @@ export const cha = [
     name: "KIRBY",
     fullname:"Kirby",
     game:"Kirby",
-    image: "",
+    image: "https://i.imgur.com/PtfOAqK.jpg",
     hint_one: "Copia o poder dos inimigos ao engolir eles",
     hint_two:"Pode ser considerado uma “bolinha rosa”",
     hint_three:"O nome do personagem está presente no título dos jogos"
@@ -111,7 +111,7 @@ export const cha = [
     name: "NOOB",
     fullname:"Noob Saibot",
     game:"Mortal Kombat",
-    image: "",
+    image: "https://i.imgur.com/uVtUfmo.png",
     hint_one: "Seu nome ao contrário é o nome do criador do jogo",
     hint_two:"Já foi banido de torneios por ser desbalanceado",
     hint_three:"Faz parte de Mortal Kombat"
@@ -120,7 +120,7 @@ export const cha = [
     name: "YOSHI",
     fullname:"Yoshi",
     game:"Super Mario",
-    image: "",
+    image: "https://i.imgur.com/roUQ8Mr.png",
     hint_one: "É um dinossauro verde",
     hint_two:"Em alguns jogos pode engolir com sua língua e cuspir projéteis",
     hint_three:"Faz parte da franquia de jogos do Mario"
@@ -129,7 +129,7 @@ export const cha = [
     name: "TAILS",
     fullname:"Tails",
     game:"Sonic the Hedgehog",
-    image: "",
+    image: "https://i.imgur.com/LUycZFy.png",
     hint_one: "Tem a capacidade de planar ou voar com seu rabo",
     hint_two:"É uma raposa amarela",
     hint_three:"Fez parte de alguns jogos do Sonic"
@@ -138,7 +138,7 @@ export const cha = [
     name: "ISAAC",
     fullname:"Isaac",
     game:"The Binding of Isaac",
-    image: "",
+    image: "https://i.imgur.com/9uGtUKy.png",
     hint_one: "Protagonista de um jogo indie famoso",
     hint_two:"Um dos vilões do jogo é sua própria mãe",
     hint_three:"Ataca com suas lágrimas"
@@ -147,7 +147,7 @@ export const cha = [
     name: "ALICE",
     fullname:"Alice",
     game:"Alice Madness Returns",
-    image: "",
+    image: "https://i.imgur.com/ljP6EBt.png",
     hint_one: "Sua arma principal é uma faca chamada Vorpal Blade",
     hint_two:"O jogo se passa num lugar não tão “maravilhoso”",
     hint_three:"Seu jogo é uma versão sombria de um livro clássico"
@@ -156,7 +156,7 @@ export const cha = [
     name: "GERALT",
     fullname:"Geralt of Rivia",
     game:"The Witcher",
-    image: "",
+    image: "https://i.imgur.com/hX4Dmwq.jpg",
     hint_one: "Ferro para humanos, prata para monstros",
     hint_two:"Protagonista de uma trilogia de RPG's",
     hint_three:"É considerado um bruxo"
@@ -165,7 +165,7 @@ export const cha = [
     name: "CHRIS",
     fullname:"Chris Redfield",
     game:"Resident Evil",
-    image: "",
+    image: "https://i.imgur.com/9JAlPUo.jpg",
     hint_one: "Membro da BSAA e combate armas biológicas",
     hint_two:"Usa armas de fogo contra “mortos vivos”",
     hint_three:"Foi protagonista do primeiro e do quinto jogo da saga"
@@ -174,7 +174,7 @@ export const cha = [
     name: "SUBZERO",
     fullname:"Sub-Zero",
     game:"Mortal Kombat",
-    image: "",
+    image: "https://i.imgur.com/SgSxQD6.png",
     hint_one: "É um ninja azul",
     hint_two:"Usa poderes de gelo",
     hint_three:"Faz parte da franquia Mortal Kombat"
@@ -183,7 +183,7 @@ export const cha = [
     name: "ZERO",
     fullname:"Zero",
     game:"Mega Man X",
-    image: "",
+    image: "https://i.imgur.com/24fAkWC.png",
     hint_one: "É um Maverick Hunter",
     hint_two:"Ataca com um sabre de luz",
     hint_three:"Apareceu pela primeira vez na saga Mega Man X"
@@ -191,7 +191,7 @@ export const cha = [
     name: "TERRY",
     fullname:"Terry Bogard",
     game:"Fatal Fury / The King Of Fighters",
-    image: "",
+    image: "https://i.imgur.com/AylqC6Z.png",
     hint_one: "Faz parte de jogos de luta",
     hint_two:"Usa sempre um boné vermelho e branco",
     hint_three:"Mais conhecido por The King of Fighters, mas não se originou lá"
@@ -199,7 +199,7 @@ export const cha = [
     name: "RUGAL",
     fullname:"Omega Rugal",
     game:"The King Of Fighters",
-    image: "",
+    image: "https://i.imgur.com/g1eDAgv.png",
     hint_one: "Considerado um dos “chefões” mais difíceis",
     hint_two:"Apelidado de “arranca ficha”",
     hint_three:"Ficou conhecido por The King of Fighters"
@@ -208,7 +208,7 @@ export const cha = [
     name: "SHAOKAHN",
     fullname:"Shao Kahn",
     game:"Mortal Kombat",
-    image: "",
+    image: "https://i.imgur.com/y92WSsy.jpg",
     hint_one: "Imperador da Exoterra (Outworld)",
     hint_two:"Um dos vilões mais icônicos dos jogos de luta",
     hint_three:"Sua frase mais famosa é “Finish him!”"
@@ -217,7 +217,7 @@ export const cha = [
     name: "MAGUS",
     fullname:"Magus",
     game:"Chrono Trigger",
-    image: "",
+    image: "https://i.imgur.com/EqnJqGC.png",
     hint_one: "É um mago sombrio",
     hint_two:"Um dos antagonistas do jogo, mas pode se unir à party",
     hint_three:"Fez parte de Chrono Trigger"
@@ -226,153 +226,162 @@ export const cha = [
     name: "JIN",
     fullname:"Jin Kazama",
     game:"Tekken",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/xOyzivQ.jpg",
+    hint_one: "Neto de Heihachi Mishima",
+    hint_two:"Tem uma forma com asas negras e chifres",
+    hint_three:"Já foi protagonista de Tekken"
 },
 { 
     name: "SAGAT",
     fullname:"Sagat",
     game:"Street Fighter",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/KQo9RPi.png",
+    hint_one: "Luta Muay Thai",
+    hint_two:"Um golpe famoso dele é o “Tiger Uppercut”",
+    hint_three:"Um dos sub chefes de Street Fighter 2"
 },
 { 
     name: "AKUMA",
     fullname:"Akuma",
     game:"Street Fighter",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/yVgNYVn.jpeg",
+    hint_one: "Lutador poderoso e secreto em alguns jogos",
+    hint_two:"Seu nome pode significar demônio em japonês",
+    hint_three:"Faz parte da franquia Street Fighter"
 },
 { 
     name: "WANDER",
     fullname:"Wander",
     game:"Shadow of the Colossus",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/qAKjy11.png",
+    hint_one: "Usa arco e flecha e uma espada",
+    hint_two:"Uma égua muito leal o acompanha",
+    hint_three:"Precisa derrotar 16 colossos"
 },
 { 
     name: "AGRO",
     fullname:"Agro",
     game:"Shadow of the Colossus",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/DNglTk5.png",
+    hint_one: "Seu nome remete a agressividade",
+    hint_two:"É um animal valente e leal",
+    hint_three:"Seu nome é chamado ao apertar X no jogo"
 },
 { 
     name: "CREEPER",
     fullname:"Creeper",
     game:"Minecraft",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/HDDbKdP.png",
+    hint_one: "“___, oh man”",
+    hint_two:"Seu poder é explodir ao chegar perto",
+    hint_three:"Faz parte de um jogo onde tudo é quadrado"
 },
 { 
     name: "CJ",
     fullname:"Carl Johnson",
     game:"Grand Theft Auto",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/7i8kVTk.png",
+    hint_one: "Protagonista de um dos jogos mais famosos de PS2",
+    hint_two:"“Ah shit, here we go again”",
+    hint_three:"Pode ser considerada a maior franquia da Rockstar Games"
 },
 { 
     name: "VEGA",
     fullname:"Vega",
     game:"Street Fighter",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/tD3d0Fn.png",
+    hint_one: "Usa uma máscara e uma garra enquanto luta",
+    hint_two:"É espanhol",
+    hint_three:"Rival de Chun-Li"
 },
 { 
     name: "CHUNLI",
     fullname:"Chun-Li",
     game:"Street Fighter",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/sAtiFBB.png",
+    hint_one: "É de nacionalidade chinesa",
+    hint_two:"Luta usando as pernas na maior parte do tempo",
+    hint_three:"Uma das personagens mais famosas de Street Fighter"
 },
 { 
     name: "WESKER",
     fullname:"Albert Wesker",
     game:"Resident Evil",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/9HjZ9p3.png",
+    hint_one: "“7 minutes is all I can spare to play with you” é uma frase famosa",
+    hint_two:"O youtuber Davy Janes confundiu seu nome com o do Krauser",
+    hint_three:"Antagonista de alguns jogos da saga Resident Evil"
 },
 { 
     name: "PRICE",
     fullname:"Captain Price",
     game:"Call of Duty Modern Warfare",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/BmWIbVS.jpg",
+    hint_one: "“Bravo Six, Going Dark” é uma frase famosa dele",
+    hint_two:"Faz parte do SAS britânico e tem alcunha de capitão",
+    hint_three:"Fez parte de alguns jogos da franqui Call of Duty"
 },
 { 
     name: "ARTHAS",
     fullname:"Arthas Menethil",
     game:"Warcraft",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/xA3JEeP.jpg",
+    hint_one: "Era príncipe de Lordaeron",
+    hint_two:"Se tornou Lich Rei e empunha a Frostmourne",
+    hint_three:"Faz parte de uma franquia famosa da Blizzard"
 },
 { 
     name: "PATCHES",
     fullname:"Patches",
     game:"Dark Souls",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/pOkJHEk.jpg",
+    hint_one: "Você não deve confiar nesse personagem",
+    hint_two:"Vai te passar a perna se você der a chance",
+    hint_three:"Aparece na saga Dark Souls"
 },
 { 
     name: "TRAPPER",
     fullname:"Trapper",
     game:"Dead by Daylight",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/vlENPFp.jpg",
+    hint_one: "Seu design foi inspirado no Jason Voorhees",
+    hint_two:"Killer que usa armadilhas de urso como habilidade principal",
+    hint_three:"Personagem icônico de Dead By Daylight"
 },
 { 
     name: "DAMA",
     fullname:"Dama Negra",
     game:"Xadrez",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/SM3yRFO.jpg",
+    hint_one: "É a personagem mais forte de seu jogo, só se iguala à sua outra versão",
+    hint_two:"Quando ela cai de graça, o jogo está praticamente perdido",
+    hint_three:"Xeque-mate!"
 },
 { 
     name: "GODOT",
     fullname:"Godot",
     game:"Ace Attorney",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/s9WGhhe.png",
+    hint_one: "Era advogado mas virou promotor",
+    hint_two:"É viciado em café e tem uma música memorável",
+    hint_three:"Antagonista de um jogo de advogados chamado Ace Attorney"
 },
 { 
     name: "RATO",
     fullname:"Rato",
     game:"Transformice",
-    image: "",
-    hint_one: "",
-    hint_two:"",
-    hint_three:""
+    image: "https://i.imgur.com/EUNqSuQ.png",
+    hint_one: "Todos os jogadores utilizam esse mesmo personagem",
+    hint_two:"Seu objetivo é pegar o queijo e levar para a toca",
+    hint_three:"Seu jogo é online, multiplayer, gratuito e exclusivo de computadores"
+},
+{ 
+    name: "AOONI",
+    fullname:"Ao Oni",
+    game:"Ao Oni",
+    image: "https://i.imgur.com/9c4EWJn.png",
+    hint_one: "Ele te persegue numa mansão assombrada",
+    hint_two:"É de um jogo de terror feito no RPG Maker",
+    hint_three:"Seu nome significa “Demônio Azul” em japonês"
 }
 ]
